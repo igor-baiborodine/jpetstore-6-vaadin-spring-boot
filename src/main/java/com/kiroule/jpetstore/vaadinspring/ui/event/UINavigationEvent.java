d47b9bb27d066577b878ec185f0d238f7b7e31a3
Igor Baiborodine <ibaiborodine@gmail.com>
2015-11-12 21:32:18 -0500

    Moved from java-various-examples repository

---
+++ src/main/java/com/kiroule/jpetstore/vaadinspring/ui/event/UINavigationEvent.java
@@ -0,0 +1,17 @@
+package com.kiroule.jpetstore.vaadinspring.ui.event;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class UINavigationEvent {
+
+  private String viewName;
+
+  public UINavigationEvent(String viewName) {
+    this.viewName = viewName;
+  }
+
+  public String getViewName() {
+    return viewName;
+  }
+}
---
+++ src/main/java/com/kiroule/jpetstore/vaadinspring/ui/util/ViewConfig.java
@@ -0,0 +1,17 @@
+package com.kiroule.jpetstore.vaadinspring.ui.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Igor Baiborodine
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ViewConfig {
+
+  String displayName();
+  String productCategory() default "";
+}
